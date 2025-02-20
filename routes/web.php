984ccd9a375f6df8ce71772350a0c98d1164d473
Abdeljalil <abdeljalilelouafi2@gmail.com>
2025-02-20 14:04:45 +0100

    Edited routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,38 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostsController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PostsController::class, 'welcome'])->name('welcome');
+
+// Route::resource('categories', CategoryController::class);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::put('/categories/{category}', [CategoryController::class, 'update']);
+Route::post('/categories/{category}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+Route::delete('/categories/{category}/force-delete', [CategoryController::class, 'forceDelete'])->name('categories.forceDelete');
+Route::get('/categories/trashed', [CategoryController::class, 'trashed'])->name('categories.trashed');
+
+
+
+Route::get('/posts/trashed', [PostsController::class, 'trashed'])->name('posts.trashed');
+Route::get('/posts/{id}/restore', [PostsController::class, 'restore'])->name('posts.restore');
+Route::delete('/posts/{id}/force-delete', [PostsController::class, 'forceDelete'])->name('posts.forceDelete');
+
 Route::middleware('auth')->group(function () {
+    Route::resource('posts', PostsController::class);
+    Route::resource('categories', CategoryController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
